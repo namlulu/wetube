790bde4c600c0453c04de27f2665e8b506d539df
sean <tmddnr3163@gmail.com>
2021-05-26 22:24:36 +0900

    2.1 Middleware, Controller

--- src/server.js
+++ src/server.js
@@ -4,11 +4,29 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url} ${req.path} `);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === '/protected') {
+    return res.send('<h1>Not Allowed</h1>');
+  }
+  console.log('Allowed, you may continue');
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send('<h1>Hello World!</h1>');
 };
 
+const handleProtected = () => {};
+
+app.use(logger, privateMiddleware);
 app.get('/', handleHome);
+app.get('/protected', handleProtected);
 
 const handleListening = () =>
   console.log(`Server listenling on port http://localhost:${PORT}`);
