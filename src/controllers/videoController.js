3f7968472ce87eea41a2863445b879ff8ee79936
sean <tmddnr3163@gmail.com>
2021-06-07 19:25:17 +0900

    5.2 CRUD with MongoDB and Mongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,71 +1,83 @@
-const videos = [
-  {
-    title: 'First Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes age',
-    views: 59,
-    id: 1,
-  },
-  {
-    title: 'Second Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes age',
-    views: 59,
-    id: 2,
-  },
-  {
-    title: 'Third Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes age',
-    views: 59,
-    id: 3,
-  },
-];
+import Video from '../models/Video';
 
-export const trending = (req, res) => {
-  res.render('home', { pageTitle: 'Home', videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({}).sort({ createdAt: 'desc' });
+    res.render('home', { pageTitle: 'Home', videos });
+  } catch (error) {
+    return res.render('server-error', { error });
+  }
 };
-
-export const watch = (req, res) => {
-  const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render('watch', { pageTitle: `Watching ${video.title}`, video });
-};
-
-export const getEdit = (req, res) => {
-  const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render('edit', { pageTitle: `Watching ${video.title}`, video });
-};
-
-export const postEdit = (req, res) => {
-  const { id } = req.params;
-  const { title } = req.body;
-  videos[id - 1].title = title;
-  return res.redirect(`/videos/${id}`);
-};
-
-export const search = (req, res) => res.send('search Video');
 
 export const getUpload = (req, res) => {
   return res.render('upload', { pageTitle: 'Uploading Video' });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  const newVideo = {
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: Video.formatHashtags(hashtags),
+    });
+    return res.redirect('/');
+  } catch (error) {
+    return res.render('upload', {
+      pageTitle: 'Upload Video',
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found' });
+  }
+  return res.render('watch', { pageTitle: video.title, video });
+};
+
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found' });
+  }
+  return res.render('edit', { pageTitle: `Edit: ${video.title}`, video });
+};
+
+export const postEdit = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found' });
+  }
+  await Video.findByIdAndUpdate(id, {
     title,
-    rating: 0,
-    comments: 0,
-    createdAt: 'just now',
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+    description,
+    hashtags: formatHashtags(hashtags),
+  });
+  return res.redirect(`/videos/${id}`);
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findOneAndDelete(id);
   return res.redirect('/');
 };
 
-export const deleteVideo = (req, res) => res.send('deleteVideo');
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, 'i'),
+      },
+    });
+  }
+  return res.render('search', { pageTitle: 'Search', videos });
+};
--- src/db.js
+++ src/db.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose';
 mongoose.connect('mongodb://127.0.0.1:27017/wooktube', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: true,
 });
 
 const db = mongoose.connection;
 
-db.on('error', (error) => console.log('DB error ?', error));
-db.once('open', () => console.log('Connect to DB ?'));
+db.on('error', (error) => console.log('DB error', error));
+db.once('open', () => console.log('Connect to DB'));
