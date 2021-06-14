b1bb81e9e37949673bfb5b68b4aa0a340b52b919
sean <tmddnr3163@gmail.com>
2021-06-14 17:00:49 +0900

    7.0 MongoDB Session and .env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
 import { localsMiddleware } from './middlewares';
+
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
@@ -15,9 +18,12 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: 'Hello!',
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URL,
+    }),
   })
 );
 
