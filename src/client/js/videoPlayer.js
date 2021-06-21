5a77a315f582f6fab4d3917428b2a47546acb56d
sean <tmddnr3163@gmail.com>
2021-06-22 00:01:53 +0900

    12.0 Video Player

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector('video');
 const playBtn = document.getElementById('play');
 const muteBtn = document.getElementById('mute');
-const time = document.getElementById('time');
+const currentTime = document.getElementById('currentTime');
+const totalTime = document.getElementById('totalTime');
 const volumeRange = document.getElementById('volume');
+const timeline = document.getElementById('timeline');
+const fullScreenBtn = document.getElementById('fullScreen');
+const videoContainer = document.getElementById('videoContainer');
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -42,6 +46,42 @@ const handleVolumnChange = (e) => {
   video.volume = value;
 };
 
+const formatTime = (seconds) => {
+  return new Date(seconds * 1000).toISOString().substr(11, 8);
+};
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeupdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  video.currentTime = value;
+};
+
+const handleFullscreenClick = (e) => {
+  const fullScreen = document.fullscreenElement;
+  if (fullScreen) {
+    document.exitFullscreen();
+    fullScreenBtn.innerText = 'Enter Full Screen';
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerText = ' Exit Full Screen';
+  }
+};
+
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
 volumeRange.addEventListener('input', handleVolumnChange);
+video.addEventListener('loadedmetadata', handleLoadedMetadata);
+video.addEventListener('timeupdate', handleTimeupdate);
+timeline.addEventListener('input', handleTimelineChange);
+fullScreenBtn.addEventListener('click', handleFullscreenClick);
