075550a780136b3165716162ba7935e905c9dd22
sean <tmddnr3163@gmail.com>
2021-07-09 14:34:06 +0900

    log for test

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -58,6 +58,7 @@ const formatTime = (seconds) =>
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration));
   timeline.max = Math.floor(video.duration);
+  console.log(video.duration);
 };
 
 const handleTimeUpdate = () => {
